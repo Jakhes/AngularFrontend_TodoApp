313b4a7e5f53a4665a610eec90f290d6f5593784
Braumble <dean.schmitz@schmitzbauer.de>
2024-07-18 16:14:53 +0200

    Creating Todo Service

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from './todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private apiServerUrl = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.apiServerUrl}/todo/all`);
+  }
+
+  public addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(`${this.apiServerUrl}/todo/add`, todo);
+  }
+
+  public updateTodo(todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.apiServerUrl}/todo/update`, todo);
+  }
+
+  public deleteTodo(todoId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/todo/delete/${todoId}`);
+  }
 }
