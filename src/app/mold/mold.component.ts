810039a9fb2d5375a7ba77b9687e23923590e763
Braumble <dean.schmitz@schmitzbauer.de>
2025-01-17 19:54:35 +0100

    Fix Aspect ratio bug of canvas

--- src/app/mold/mold.component.ts
+++ src/app/mold/mold.component.ts
@@ -156,7 +156,7 @@ export class MoldComponent implements AfterViewInit, OnDestroy {
 
       p.setup = () => {
         if (this.container) {
-          p.createCanvas(width, width).parent(this.container.nativeElement);
+          p.createCanvas(width, height).parent(this.container.nativeElement);
           p.background(20);
           p.angleMode('degrees');
           d = p.pixelDensity();
